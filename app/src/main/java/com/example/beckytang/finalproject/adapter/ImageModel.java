fd8688bd4de6361d791e5227070b6fed479acfc3
Becky Tang <beckytang@Beckys-MacBook-Air.local>
2016-12-08 17:12:32 +0100

    Began recycler adapter

---
+++ app/src/main/java/com/example/beckytang/finalproject/adapter/ImageModel.java
@@ -0,0 +1,31 @@
+package com.example.beckytang.finalproject.adapter;
+
+/**
+ * Created by beckytang on 12/8/16.
+ */
+
+public class ImageModel {
+    String photoName;
+    String url;
+
+    public ImageModel() {
+
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPhotoName() {
+        return photoName;
+    }
+
+    public void setPhotoName(String photoName) {
+        this.photoName = photoName;
+    }
+
+}
